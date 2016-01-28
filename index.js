3e52efbea88bc8b47ace5ba7d8963f8cccd8c183
Juan Soto <juan@juansoto.me>
2016-01-28 09:45:57 -0500

    feat: Add secsToMin function

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
 'use strict';
 
-module.exports = function (str, opts) {
-  if (typeof str !== 'string') {
-    throw new TypeError('Expected a string');
+const secsToMin = seconds => {
+  let secs = Math.round(seconds);
+
+  if (secs < 60) {
+    if (secs < 10) return `0:0${secs}`;
+
+    return `0:${secs}`;
   }
 
-  opts = opts || {};
+  let minuteDivisor = secs % (60 * 60);
+  let minutes = Math.floor(minuteDivisor / 60);
 
-  return 'I am cool';
+  let secondDivisor = minuteDivisor % 60;
+  let remSecs = Math.ceil(secondDivisor);
+
+  if (remSecs < 10 && remSecs > 0) remSecs = `0${remSecs}`;
+  if (remSecs === 0) remSecs = `${remSecs}0`;
+
+  let time = {
+    m: minutes,
+    s: remSecs
+  };
+
+  return `${time.m}:${time.s}`;
 };
+
+module.exports = secsToMin;
--- test.js
+++ test.js
@@ -2,5 +2,11 @@ import test from 'ava';
 import fn from './';
 
 test('fn', t => {
-	t.is(fn('Hello'), 'I am cool');
+  t.is(fn(60), '1:00');
+  t.is(fn(180), '3:00');
+  t.is(fn(185), '3:05');
+  t.is(fn(70), '1:10');
+  t.is(fn(71), '1:11');
+  t.is(fn(1), '0:01');
+	t.is(fn(0), '0:00');
 });
