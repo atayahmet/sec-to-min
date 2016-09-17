577f71a8237c0ae459d3d89b14265da26a84477d
Juan Soto <juan@juansoto.me>
2016-09-17 13:29:41 -0500

    Switch AVA for Tape
    This is a tiny module, so I'd rather it install quickly on CIs/other
    devs computers.

--- test.js
+++ test.js
@@ -1,7 +1,9 @@
-import test from 'ava';
-import fn from './';
+'use strict';
 
-test('converts seconds to minutes', t => {
+const test = require('tape');
+const fn = require('./');
+
+test('converts seconds to minutes', (t) => {
   t.is(fn(60), '1:00');
   t.is(fn(180), '3:00');
   t.is(fn(185), '3:05');
@@ -9,13 +11,17 @@ test('converts seconds to minutes', t => {
   t.is(fn(71), '1:11');
   t.is(fn(1), '0:01');
   t.is(fn(0), '0:00');
+
+  t.end();
 });
 
-test('throws error on negative seconds', t => {
+test('throws error on negative seconds', (t) => {
   try {
     fn(-1);
   } catch (error) {
-    t.truthy(error);
-    t.is(error.message, 'Seconds must be positive')
+    t.ok(error);
+    t.is(error.message, 'Seconds must be positive');
+
+    t.end();
   }
 });
